f76b08a8461d4679c307ea2d73f7d6e186df601f
unknown <sasikumar.9799@gmail.com>
2023-03-19 14:24:46 +0530

    server

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl:'https://gurustockmanagement.onrender.com/'
+  baseUrl:'https://gurustockmanagement-i1jq.onrender.com/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  baseUrl:'https://gurustockmanagement.onrender.com/'
+  baseUrl:'https://gurustockmanagement-i1jq.onrender.com/'
 };
 
 /*
