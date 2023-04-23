eeade7736f5cf41a2df8dd8bb717ca7b8d7465e4
unknown <sasikumar.9799@gmail.com>
2023-04-23 19:18:27 +0530

    changes

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,7 +4,7 @@ const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'firstProjectIonic',
   webDir: 'www',
-  bundledWebRuntime: false
+  bundledWebRuntime: true
 };
 
 export default config;
