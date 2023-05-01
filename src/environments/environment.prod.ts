0312910f7be5cfaff8432e307999cdf32b2c2d07
unknown <sasikumar.9799@gmail.com>
2023-05-01 13:41:04 +0530

    new server

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl:'https://gurustockmanagement-i1jq.onrender.com/'
-  // baseUrl:'http://localhost:3000/'
+  // baseUrl:'https://gurustockmanagement-i1jq.onrender.com/'
+  baseUrl:'http://localhost:3000/'
 };
